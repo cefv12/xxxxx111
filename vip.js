4f30705ab5ec49b240cbf65dcbf5c6bda7eda677
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-01-21 22:41:32 +0800

    Update vip.js

--- vip.js
+++ vip.js
@@ -1,7 +1,81 @@
 var obj = JSON.parse($response.body); 
 // 可以合并一句带过
 obj = {
-    "user" : {
+  "retcode" : 0,
+  "errmsg" : "",
+  "data" : {
+    "signed" : 0,
+    "groups" : [
+      {
+        "minup" : "0",
+        "gicon" : "V0",
+        "gname" : "游客",
+        "play_daynum" : 15,
+        "down_daynum" : 0,
+        "comment_daynum" : 0
+      },
+      {
+        "minup" : "0",
+        "gicon" : "V1",
+        "gname" : "VIP1",
+        "play_daynum" : 25,
+        "down_daynum" : 4,
+        "comment_daynum" : 5
+      },
+      {
+        "minup" : "2",
+        "gicon" : "V2",
+        "gname" : "VIP2",
+        "play_daynum" : 50,
+        "down_daynum" : 10,
+        "comment_daynum" : 10
+      },
+      {
+        "minup" : "5",
+        "gicon" : "V3",
+        "gname" : "VIP3",
+        "play_daynum" : 100,
+        "down_daynum" : 20,
+        "comment_daynum" : 15
+      },
+      {
+        "minup" : "10",
+        "gicon" : "V4",
+        "gname" : "VIP4",
+        "play_daynum" : 200,
+        "down_daynum" : 40,
+        "comment_daynum" : 20
+      },
+      {
+        "minup" : "20",
+        "gicon" : "V5",
+        "gname" : "尊贵VIP",
+        "play_daynum" : 999,
+        "down_daynum" : 100,
+        "comment_daynum" : 30
+      }
+    ],
+    "uinfo" : {
+      "curr_group" : {
+        "minup" : "20",
+        "gicon" : "V5",
+        "gid" : "5",
+        "gname" : "尊贵VIP"
+      },
+      "next_group" : {
+        "minup" : "65535",
+        "gicon" : "",
+        "gid" : "6",
+        "gname" : "禁止发言"
+      },
+      "next_upgrade_need" : 65535,
+      "down_daily_remainders" : 100,
+      "play_daily_remainders" : 984,
+      "goldcoin" : "1"
+    },
+    "reqplay_addnum" : "0",
+    "reqdown_addnum" : "0",
+   "user" : {
       "duetime" : "9999-01-22 13:43:38",
       "sysgid" : "5",
       "uid" : "10357243",
@@ -22,6 +96,9 @@ obj = {
       "isvip" : 1,
       "gender" : "0"
     }
+  }
+}
+
     // 有需要全部替换的情况，只需让用户在这里如此操作即可
 
 $done({body:JSON.stringify(obj)});
