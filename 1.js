6a876cfab6ee42b2bda8ed6a5d756e60fdac040d
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-01-21 22:31:32 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,25 +1,408 @@
 var obj = JSON.parse($response.body); 
 // 可以合并一句带过
 obj = {
-  "sdkrows_iOS" : [
+{
+  "retcode" : 0,
+  "errmsg" : "",
+  "data" : {
+    "app_launch_times_adshow" : 0,
+    "hotwords" : [
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      },
+      {
+        "dspwd" : "古装",
+        "schwd" : "古装"
+      }
+    ],
+    "popupactivity_iOS" : null,
+    "sharetext" : "",
+    "sharetaskbox" : {
+      "title" : "",
+      "picurl" : "",
+      "intro" : "",
+      "link" : ""
+    },
+    "Android_adgroups" : {
+      "global_adgroup_ad7" : [
+        {
+          "url" : "https://cdn.xahxlh.com/xxgame/",
+          "countdown" : "0",
+          "pic" : "https://pic.xjpik.com/202112/3d/361aa6f1bba2b9306cc10eae5c1cc93d.gif"
+        },
+        {
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index02.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202201/46/661e6fefa76e44357679bd23586d0846.gif"
+        },
+        {
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index02.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/a6/961ce92cf54ce990231af80087451da6.gif"
+        },
+        {
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index01.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/26/361b6f12339fcd222f7b7e8f4613ec26.gif"
+        },
+        {
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index01.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/3f/761a9f3a869a03514304f23e0219433f.gif"
+        }
+      ],
+      "global_adgroup_ad80" : [
+        {
+          "pic" : "https://ad2.qiyedaoshi.com/20210405/WDz7kPS2/mp4/WDz7kPS2.mp4",
+          "url" : "https://dl.padfc.com/promotion/index_n817_6.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "skip" : "1"
+        },
+        {
+          "pic" : "https://ad2.qiyedaoshi.com/20210405/RSj6slpt/mp4/RSj6slpt.mp4",
+          "url" : "https://dl.hljdmm.com/hszz2.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "skip" : "1"
+        }
+      ],
+      "global_adgroup_ad1" : [
+        {
+          "url" : "https://boao8322.com:32202",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/35/861bac6884138924b01ab34b41fa1835.jpg"
+        },
+        {
+          "url" : "https://boao8322.com:32202",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/99/561bac693d61e00166366395db012999.jpg"
+        },
+        {
+          "url" : "https://boao8322.com:32202",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/db/761bac6a06ab7d54bee3dc0ec24d65db.jpg"
+        },
+        {
+          "url" : "https://boao8322.com:32202",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/a6/261bac6ac5310705b656a194139269a6.jpg"
+        },
+        {
+          "url" : "https://boao8322.com:32202",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/12/761bac6b837a1e15c8baae00c196cf12.jpg"
+        }
+      ],
+      "global_adgroup_ad8" : [
+        {
+          "pic" : "https://vv09.qiyedaoshi.com/20220118/2y4v0WvU/index.m3u8",
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index02.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "skip" : "1"
+        },
+        {
+          "pic" : "https://vv09.qiyedaoshi.com/20220118/qLEBTr2y/index.m3u8",
+          "url" : "https://dl.yongchuanba.com:10843/promotion/?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "skip" : "1"
+        },
+        {
+          "pic" : "https://vv09.qiyedaoshi.com/20211230/ZJAwZgrB/index.m3u8",
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index01.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "skip" : "1"
+        },
+        {
+          "pic" : "https://vv09.qiyedaoshi.com/20211230/gYCvy3ZJ/index.m3u8",
+          "url" : "https://dl.yongchuanba.com:10843/promotion/?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "skip" : "1"
+        },
+        {
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index02.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202201/ba/061e6fe8990618525871b123cd2b56ba.gif"
+        },
+        {
+          "url" : "https://dl.yongchuanba.com:10843/promotion/index01.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202112/49/361ce91859f0a61422b505e6a2fccf49.gif"
+        }
+      ],
+      "global_adgroup_ad16" : []
+    },
+    "newurl" : "www.xiao1.app",
+    "appdownurl" : "http://www.wangdunwen.com/?inviteCode=LL1JOV",
+    "promotion_earn_dscr" : {
+      "point2_text" : "1",
+      "point2_coin" : 1,
+      "illustrate" : "1",
+      "point1_coin" : 2,
+      "point1_text" : "2"
+    },
+    "popuptext_v2" : {
+      "link" : "最新官网：Xiao1.APP",
+      "text" : "IOS预防丢失请下载我们桌面版噢！\n打造最好的影视资源分享平台！\n觉得好用请分享给朋友们噢！"
+    },
+    "hottags" : [
+      "动作",
+      "惊悚",
+      "冒险"
+    ],
+    "qrlink" : "http://www.llttx.com/?inviteCode=LL1JOV",
+    "shareregister" : {
+      "title" : "小小影视APP要你好看，永久免费在线观看，支持投屏、离线观影，网罗海内外影视资源！下载用戳↓↓↓",
+      "picurl" : "https://img.ynkmjj.com/201911/38/55ddfff4d73633604b46778f8e286038.png",
+      "intro" : "小小影视vip影片免费看！",
+      "link" : "http://www.llttx.com/?inviteCode=LL1JOV"
+    },
+    "popuptext" : "",
+    "sdkrows_Android" : [
       {
         "title" : "启动页",
         "pic" : "",
         "pos" : "",
-        "sdkid" : "0"
+        "sdkid" : "1"
       },
       {
         "title" : "播放前贴片",
         "pic" : "",
         "pos" : "",
-        "sdkid" : "0"
+        "sdkid" : "1"
+      },
+      {
+        "title" : "详情页标题上方",
+        "pic" : "",
+        "pos" : "",
+        "sdkid" : "1"
+      },
+      {
+        "title" : "详情页播放列表下方",
+        "pic" : "",
+        "pos" : "",
+        "sdkid" : "1"
+      }
+    ],
+    "sdkrows_iOS" : [
+      {
+        "title" : "启动页",
+        "pic" : "",
+        "pos" : "",
+        "sdkid" : "1"
+      },
+      {
+        "title" : "播放前贴片",
+        "pic" : "",
+        "pos" : "",
+        "sdkid" : "1"
       },
       {
         "title" : "详情页下方",
         "pic" : "",
         "pos" : "",
-        "sdkid" : "0"
+        "sdkid" : "1"
       }
+    ],
+    "popuptext_Android" : {
+      "link" : "永久官网：xiao1.app",
+      "urltext" : "点我进入官网下载最新版本",
+      "url" : "http://www.by1617.com",
+      "text" : "【李茂扮太子】【反贪风暴5：最终章】【穿过寒冬拥抱你】【误杀2】【蜘蛛侠：英雄无归】【永恒族】【黑客帝国：矩阵重启】已经更新，欢迎观看，点赞，转发！\n近期部分影片在调整线路，线路播放不了的可以手动切换其他线路观看！\n注册账号即送至尊Vip，秒跳广告，无限次数观看！！\n一个账号只能登录一个设备，请勿多账号登录。"
+    },
+    "hotcategories" : [
+      {
+        "url" : "/vod/listing-0-0-0-0-0-0-2-0-0-0",
+        "catename" : "电视剧"
+      },
+      {
+        "url" : "/vod/listing-4-0-0-0-0-0-2-0-0-0",
+        "catename" : "电影"
+      }
+    ],
+    "hotpoint" : {
+      "enable" : 1,
+      "url" : "http://t.mokayd.com/s/LJ27u6BZya"
+    },
+    "potatolink" : "https://open.clickai.cn/im/group/invite/?groupid=126626&schemeName=inviteToGroup&validate=1640928401014&userid=152083",
+    "fastdown" : {
+      "enable" : 1
+    },
+    "popuptext_iOS" : {
+      "link" : "永久官网：xiao1.app",
+      "urltext" : "点我进入官方网下载最新版本",
+      "url" : "http://www.by1617.com",
+      "text" : "【李茂扮太子】【反贪风暴5：最终章】【穿过寒冬拥抱你】【误杀2】【蜘蛛侠：英雄无归】【永恒族】【黑客帝国：矩阵重启】已经更新，欢迎观看，点赞，转发！\n近期部分影片在调整线路，线路播放不了的可以手动切换其他线路观看！\n注册账号即送至尊Vip，秒跳广告，无限次数观看！！\n一个账号只能登录一个设备，请勿多账号登录。\n部分苹果用户无法投屏请点击底部链接下载最新版本解决。"
+    },
+    "popupactivity_Android" : null,
+    "app_launch_type_adshow" : {
+      "iOS" : 0,
+      "Android" : 0
+    },
+    "adgroups" : {
+      "global_adgroup_ad7" : [
+        {
+          "url" : "https://dl.dujuanqing.com/promotion/index_n817_2.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202105/c4/26092de7cc05cd034bfcef58c12203c4.gif"
+        },
+        {
+          "url" : "https://dl.dujuanqing.com/promotion/index_n817_3.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202105/6f/76092de8f9a5bc52a4710aaed8de206f.gif"
+        },
+        {
+          "url" : "https://dl.dujuanqing.com/promotion/index_n817_3.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202104/60/860826ee54c777044f511c289878d360.gif"
+        },
+        {
+          "url" : "https://dl.dujuanqing.com/promotion/index_n817_4.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202104/63/360786e309bec250d9ecc6fccf193363.gif"
+        },
+        {
+          "url" : "https://dl.dujuanqing.com/promotion/index_n817_5.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202104/24/2608c260325bde45193313f59afe0d24.gif"
+        },
+        {
+          "url" : "https://dl.dujuanqing.com/hszz2.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202104/eb/0608c26084bb3383c1bc5a145b083deb.gif"
+        }
+      ],
+      "global_adgroup_ad4" : [
+        {
+          "url" : "https://www.leitingapp2.com/i/KWKX3H",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202005/95/75ecb63f0e9f5a513e660e07fb4e0495.png"
+        }
+      ],
+      "global_adgroup_ad1" : [
+        {
+          "url" : "https://dl.padfc.com/promotion/index_n817_6.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202103/62/66043ae1c6c651356b39000e1c8c9562.jpg"
+        },
+        {
+          "url" : "https://dl.padfc.com/promotion/index_n817_2.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202103/80/46043ae243b16c91608b6f24a8b8b480.jpg"
+        },
+        {
+          "url" : "https://dl.padfc.com/promotion/index_n817_3.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "6",
+          "pic" : "https://img.yuanmifang.com/202103/09/36043ae2a0b7a503fdaefa0d6b3a2609.jpg"
+        },
+        {
+          "url" : "https://dl.padfc.com/promotion/index_n817_4.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202103/9a/660523b0862c8045766138e9e810bc9a.jpg"
+        },
+        {
+          "url" : "https://dl.padfc.com/promotion/index_n817_5.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "6",
+          "pic" : "https://img.yuanmifang.com/202103/09/660523b0d343529311b8635ac297e109.jpg"
+        },
+        {
+          "url" : "https://dl.padfc.com/promotion/index_n817_1.html?params=eyJwcm94eV9pZCI6MTEwMDA5fQ==&proxy_id=110009",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202102/b0/86036830cb1ee72386b6a30b702853b0.jpg"
+        }
+      ],
+      "global_adgroup_ad8" : [
+        {
+          "url" : "https://csydtg.980cje.com/r/90/10921.html",
+          "countdown" : "0",
+          "pic" : "https://cscdn1.39d83s.com/b/11/3155/231021/530X300.gif"
+        },
+        {
+          "url" : "https://csydtg.980cje.com/r/2d/10920.html",
+          "countdown" : "0",
+          "pic" : "https://cscdn1.39d83s.com/b/11/3155/231020/530X300.gif"
+        }
+      ],
+      "global_adgroup_ad80" : [
+        {
+          "url" : "https://dl.lyfucan.com/?params=eyJwcm94eV9pZCI6MTAwMDQ1fQ==&proxy_id=100045",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202008/d8/95f32cc5f8238642f4f4dbec8b3351d8.jpg"
+        },
+        {
+          "url" : "https://dl.lyfucan.com/?params=eyJwcm94eV9pZCI6MTAwMDQ1fQ==&proxy_id=100045",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202008/70/25f32cc6bdcd5d0433174884ae1a8070.jpg"
+        },
+        {
+          "url" : "https://down.fuyoudll.com/fy_d60233/",
+          "countdown" : "0",
+          "pic" : "https://img.yuanmifang.com/202008/fd/75f3384f6be2c67352aa693fe0053efd.jpg"
+        }
+      ],
+      "global_adgroup_ad16" : [
+        {
+          "title" : "陌陌",
+          "pic" : "https://img.huaerdadi.com/202002/f7/25e3b025b72e597076f799d5d8ea29f7.gif",
+          "url" : "https://aad.megtg.com/ad/index.html?params=eyJwcm94eV9pZCI6MTEwMDA4fQ==",
+          "intro" : "陌陌是一款约炮软件"
+        }
+      ]
+    },
+    "appver" : {
+      "AppStoreUrls" : [
+        "http://ios.51czyd.com/kaoshi/"
+      ],
+      "tfTips" : "你的APP版本即将过期，请立即更新否则无法观看",
+      "tfUrl" : "http://ios.51czyd.com/kaoshi/",
+      "AppStor1s" : "发现重要新版本，请马上升级吧！\n1、新增加速域名，提高软件稳定性。\n2、修复已知bug\n重要提示：旧版本将停止维护，届时将无法观看，请尽快升级最新版本，造成不便敬请谅解!",
+      "tfForce" : 1,
+      "AppStoreVer" : "3.2.3",
+      "AndroidForce" : 1,
+      "AndroidUrls" : [
+        
+      ],
+      "iOSForce" : 0,
+      "iOSUrl" : "",
+      "AndroidTips" : "",
+      "iOSVer" : "3.2.3",
+      "iOSUrls" : [
+        "http://ios.51czyd.com/kaoshi/"
+      ],
+      "iOSTips" : "发现重要新版本，请马上升级吧！\n1、新增加速域名，提高软件稳定性。\n2、修复已知bug\n重要提示：旧版本将停止维护，届时将无法观看，请尽快升级最新版本，造成不便敬请谅解!",
+      "AndroidUrl" : "http://www.by1617.com",
+      "AndroidVer" : "3.2.1",
+      "AppStoreMinVer" : "0.9.0"
+    }
+  }
+}
 }
 // 有需要全部替换的情况，只需让用户在这里如此操作即可
 
