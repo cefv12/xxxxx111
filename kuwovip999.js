7f7ed58a7703cc2edeca8af74c0567586e235b46
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-03-15 17:10:01 +0800

    Update kuwovip999.js

--- kuwovip999.js
+++ kuwovip999.js
@@ -29,10 +29,10 @@ const vip = '/vip/v2/user/vip';
 const time = '/vip/spi/mservice';
 
 if (url.indexOf(vip) != -1) {
-	obj.data["isNewUser"] = "2";
+	obj.data["isNewUser"] = "5";
 	obj.data["vipLuxuryExpire"] = "4165516800000";
 	obj.data["time"] = "1961170340993";
-	obj.data["isYearUser"] = "2";
+	obj.data["isYearUser"] = "5";
 	obj.data["vipmExpire"] = "4165516800000";
 	obj.data["vipOverSeasExpire"] = "4165516800000";
 	obj.data["vipExpire"] = "4165516800000";
@@ -41,8 +41,8 @@ if (url.indexOf(vip) != -1) {
 }
 
 if (url.indexOf(time) != -1) {
-	obj["isVIPMAutoPay"] = 2;
-	obj["isVIPLuxAutoPay"] = 2;
+	obj["isVIPMAutoPay"] = 5;
+	obj["isVIPLuxAutoPay"] = 5;
 	body = JSON.stringify(obj);
 }
 
