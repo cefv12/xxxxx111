f697e5a025b253983730961154ea5d62af2a76af
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-08-30 16:37:39 +0800

    Update kuwovip998.js

--- kuwovip998.js
+++ kuwovip998.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/cefv12/xxxxx111/999/kuwovip998.js
+^https?:\/\/vip1\.kuwo\.cn\/vip\/v2\/user\/vip*   url script-response-body https://raw.githubusercontent.com/cefv12/xxxxx111/999/kuwovip998.js
 
 [mitm]
 hostname = vip1.kuwo.cn
@@ -21,7 +21,7 @@ hostname = vip1.kuwo.cn
 
 **************************/
 var body = JSON.parse($response.body);
-var obj = {
+var obj = 
 {
   "data": {
     "luxAutoPayUser": "0",
