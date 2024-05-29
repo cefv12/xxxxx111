b04d3b38beae6dc0e720dd8f8eed82e7e82fb2f7
cefv12 <82319688+cefv12@users.noreply.github.com>
2024-05-29 16:28:44 +0800

    Update teai.js

--- teai.js
+++ teai.js
@@ -22,8 +22,10 @@ const vip = '/apa/user/myUserInfo';
 if (url.indexOf(vip) != -1) {
 	obj["is_vip"] = 1;
 	obj["authentication"] = 1;
-	obj["is_vip"] = 1;
-	obj["is_vip"] = 1;
+	obj["vip_end_time"] = 1937129600;
+	obj["coin"] = 991;
+	obj["viewMeCount"] = 91;
+	obj["haveReadFireCount"] = 81;
 	body = JSON.stringify(obj);
 }
 
